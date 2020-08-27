627a47915e219bde42135001a1ad4c556a3934f4
mmarchal <marchal.maxime@live.fr>
2020-08-27 21:23:56 +0200

    paiement ok style a ajuster

--- lib/paiement.dart
+++ lib/paiement.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:mia_paiement/menu.dart';
 
 class Paiement extends StatefulWidget {
   Paiement({Key key}) : super(key: key);
@@ -12,6 +14,9 @@ class Paiement extends StatefulWidget {
 class _Paiement extends State<Paiement> {
 
   String _scanBarcode = 'Unknown';
+  Produit produit = new Produit();
+
+  List<Produit> produitsList = new List();
 
   // Platform messages are asynchronous, so we initialize in an async method.
   Future<void> scanBarcodeNormal() async {
@@ -33,6 +38,7 @@ class _Paiement extends State<Paiement> {
     setState(() {
       _scanBarcode = barcodeScanRes;
     });
+    this.analyseQrCode(_scanBarcode);
   }
 
   @override
@@ -40,28 +46,114 @@ class _Paiement extends State<Paiement> {
     // TODO: implement initState
     super.initState();
     this.scanBarcodeNormal();
+
   }
 
   @override
   Widget build(BuildContext context) {
+    print(produitsList.length);
     return Scaffold(
-      body: Center(
+      body: Container(
+        margin: EdgeInsets.only(top: 50),
         child: Column(
           mainAxisSize: MainAxisSize.min,
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
-            Text('Scan result : $_scanBarcode\n',
-                style: TextStyle(fontSize: 20)),
-            RaisedButton(
-                onPressed: () {
-                  this.scanBarcodeNormal();
-                },
-              child: Text("Scanner à nouveau"),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Center(child: Icon(Icons.shopping_basket, size: 64.0, color: Colors.grey,),),
+                Container(
+                  margin: EdgeInsets.all(10),
+                  child: Text("Panier MIA BIJOUX", style: GoogleFonts.openSansCondensed(fontWeight: FontWeight.bold, color: Colors.grey), textScaleFactor: 1.7,),
+                ),
+                Center(child: Icon(Icons.shopping_basket, size: 64.0, color: Colors.grey,),),
+              ],
+            ),
+            new Flexible(child: GridView.builder(
+                itemCount: produitsList.length,
+                gridDelegate:
+                new SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2),
+                itemBuilder: (BuildContext context, int index) {
+                  Produit p = produitsList[index];
+                  return Card(
+                    elevation: 10,
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Text(p.nomProduit),
+                        Text(p.prixProduit)
+                      ],
+                    ),
+                  );
+                })),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                RaisedButton(
+                  onPressed: () {
+                    Navigator.push(context, new MaterialPageRoute(builder: (BuildContext bC) {
+                      return new Menu();
+                    }));
+                  },
+                  child: Text("Retour au menu"),
+                ),
+                RaisedButton(
+                  onPressed: () {
+                    this.scanBarcodeNormal();
+                  },
+                  child: Text("Scanner à nouveau"),
+                ),
+                RaisedButton(
+                  onPressed: () {
+                    setState(() {
+                      produitsList.clear();
+                    });
+                  },
+                  child: Text("Vider le panier"),
+                )
+              ],
             )
           ],
         ),
       ),
     );
   }
+
+  void analyseQrCode(String scanBarcode) {
+
+    String resultat = scanBarcode.replaceAll("Ajouter aux favoris", ";").replaceAll("Note", " Note").replaceAll("Epuisé", ";").replaceAll(" ", ";");
+    List<String> liste = resultat.split(";");
+
+    for( int i=0; i<liste.length; i++ ) {
+
+      String element = liste[i];
+
+      if(element=="Boucles" || element=="Bague" || element=="Bracelet" || element=="Collier") {
+        produit.nomProduit = element + " " + liste[i+1];
+      } else if (element.contains("€")) {
+        produit.prixProduit = element;
+      }
+
+    }
+
+    produitsList.add(produit);
+
+  }
+
+}
+
+class Produit {
+
+  String nomProduit;
+  String prixProduit;
+
+  @override
+  String toString() {
+    return 'Produit{nomProduit: $nomProduit, prixProduit: $prixProduit}';
+  }
+
 
 }
